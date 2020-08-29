7656fed34fd2571b5e696122cd18a73b266a774b
Michelle Barker <barkerwebdev47@gmail.com>
2020-08-28 20:54:53 -0700

    testing git

--- Pointers_Ex2/src/Test3.cpp
+++ Pointers_Ex2/src/Test3.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name : 8-16.cpp
-// Author : Paul Hrycewicz
+// Author :
 // Version :
 // Copyright : Your copyright notice
 // Description : Hello World in C++, Ansi-style
